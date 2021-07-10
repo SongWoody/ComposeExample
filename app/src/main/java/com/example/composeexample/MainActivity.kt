e9accf31b3139d6c9eb6a8f3f420045eec69d2eb
Woody <syg1138@gmail.com>
2021-07-10 22:55:27 +0900

    adds LazyColumn

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.composeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.toSize
 import com.example.composeexample.ui.theme.ComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,8 +22,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeExampleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("My Compose")
                 }
             }
         }
@@ -26,13 +34,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Scaffold(
+        modifier = Modifier.fillMaxWidth(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "HI $name") },
+                backgroundColor = Color(0xfff25287),
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Text(text = "X")
+            }
+        }
+    ) {
+        MyCompose()
+    }
+}
+
+@Composable
+fun MyCompose() {
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        items((1..1000).toList()) {
+            Text(text = "Item $it")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeExampleTheme {
-        Greeting("Android")
+        Greeting("My Compose")
     }
 }
